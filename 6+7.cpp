438045e69217ccf46c929887e2f0991314e8c0ae
Adri <adricadenas23rgmail.com>
2022-03-04 08:47:14 +0100

    Añado 6+7

---
+++ 6+7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+	int resta, num1 , num2 ;
+	std::cout << "Introduce un numero";
+	std::cin >> num1;
+	std::cout <<"Intrduce un numero";
+	std::cin >> num2;
+	resta =  num1 - num2;
+	std::cout <<"la resta de tus numeros es: " << resta;
+	
+	return 0;}
+	
+	
+
