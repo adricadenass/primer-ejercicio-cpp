d62ef29f3b56432f5215a6394ae71690bb6c9b13
adricadenass <adricadenas23rgmail.com>
2022-05-03 13:18:54 +0200

    Añad resumen

---
+++ plantillaexamen.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int variableEntera;
+	float variableConDecimales;
+	double variabaleConMuchosDecimales;
+	int varSw;
+	
+	
+	cout << "Un Texto" << varibleEntera << "Otro Texto";
+	cin >> otravariable;
+	
+	if(vriableConDecimales > OtraVariable){
+		//Esto se hace si la condicion es cierta.
+}else if (OtraVariable > variableEntera){
+	} else {
+		//Esto se hace si la condicion no es cierta.
+	}
+
+    cout << "Dame un Numero "
+    cin >> varSw
+    
+    switch (varSw){
+    	case 0:
+    	case 1:
+    		//Esto se hace si varsw e igual a 1 o 0
+    		break;
+    	case 2: //Esto se hace si varsaw es igual a 2
+    	    break;
+    	default;
+    	    break;
+    	    
+	}
+	
+	
+	while (varSw>-1){
+		//Esto se hará en bucle hasta que no sea cierta la condicion
+		cout << "Dame un numero"
+		cin >> varSw;
+	}
+
+
+	return 0;
+}
+
